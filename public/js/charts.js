d94d12c2d07e389245bcb57750456b7d90409139
Trey Thomas <TreyThomas93@gmail.com>
2020-05-21 13:26:47 -0500

    Added more graphs

--- public/js/charts.js
+++ public/js/charts.js
@@ -3,10 +3,16 @@ class Charts {
     this.liveWorkloadBarChart;
     this.averageBarChartOne;
     this.averageBarChartTwo;
+    this.averageBarChartThree;
+    this.averageBarChartFour;
+    this.averageBarChartFive;
 
     this.createLiveWorkloadBarChart();
     this.createAverageBarChartOne();
     this.createAverageBarChartTwo();
+    this.createAverageBarChartThree();
+    this.createAverageBarChartFour();
+    this.createAverageBarChartFive();
   }
 
   // Live Workload Bar Chart
@@ -189,7 +195,7 @@ class Charts {
     });
   }
 
-  // Average Post Time, Drive Time, On Call Time
+  // Post Time, Drive Time, On Call Time
   createAverageBarChartOne() {
     const ctx = document.querySelector("#average-chart-one").getContext("2d");
 
@@ -246,7 +252,7 @@ class Charts {
             },
             scaleLabel: {
               display: true,
-              labelString: "Time",
+              labelString: "Time(Minutes)",
               fontColor: "white",
               fontSize: 15,
             },
@@ -302,12 +308,12 @@ class Charts {
     //
 
     // Update Chart with new data
-    this.averageBarChartOne.data.datasets[1].data.push(
+    this.averageBarChartOne.data.datasets[0].data.push(
       currentOnCallValue,
       currentPostTimeValue,
       currentDriveTimeValue
     );
-    this.averageBarChartOne.data.datasets[0].data.push(
+    this.averageBarChartOne.data.datasets[1].data.push(
       currentOnCallAverage,
       currentPostTimeAverage,
       currentDriveTimeAverage
@@ -320,7 +326,7 @@ class Charts {
     this.averageBarChartOne.update();
   }
 
-  // Average Late Call, Past EOS
+  // Late Call, Past EOS
   createAverageBarChartTwo() {
     const ctx = document.querySelector("#average-chart-two").getContext("2d");
 
@@ -371,13 +377,14 @@ class Charts {
               fontSize: 12,
               beginAtZero: true,
               fontColor: "white",
+              max: 15,
             },
             gridLines: {
               color: "rgba(255, 255, 255, 0.25)",
             },
             scaleLabel: {
               display: true,
-              labelString: "Time",
+              labelString: "Value",
               fontColor: "white",
               fontSize: 15,
             },
@@ -430,11 +437,11 @@ class Charts {
     //
 
     // Update Chart with new data
-    this.averageBarChartTwo.data.datasets[1].data.push(
+    this.averageBarChartTwo.data.datasets[0].data.push(
       currentLateCallValue,
       currentPastEOSValue
     );
-    this.averageBarChartTwo.data.datasets[0].data.push(
+    this.averageBarChartTwo.data.datasets[1].data.push(
       currentLateCallAverage,
       currentPastEOSAverage
     );
@@ -444,6 +451,361 @@ class Charts {
       "rgba(46, 78, 100, 1)";
 
     this.averageBarChartTwo.update();
+  }
+
+  // Calls
+  createAverageBarChartThree() {
+    const ctx = document.querySelector("#average-chart-three").getContext("2d");
+
+    // Chart Data Display
+    let chartData = {
+      labels: ["Calls"],
+      datasets: [
+        {
+          label: "Current Value",
+          backgroundColor: [],
+          data: [],
+        },
+        {
+          label: "Current Average",
+          backgroundColor: [],
+          data: [],
+        },
+      ],
+    };
+
+    // Chart Data Options
+    let chartOptions = {
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              fontColor: "white",
+            },
+            barPercentage: 0.7,
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: false,
+              labelString: "Categories",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              beginAtZero: true,
+              fontColor: "white",
+              max: 250,
+            },
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Calls",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+      },
+      animation: false,
+      elements: {
+        line: {
+          tension: 0,
+        },
+      },
+      title: {
+        display: true,
+        text: "Calls",
+        fontSize: 15,
+        fontColor: "white",
+      },
+      legend: {
+        labels: {
+          //   filter: function (item, chart) {
+          //     // Logic to remove a particular legend item goes here
+          //     return item.text == null || !item.text.includes("Current Value");
+          //   },
+          fontColor: "white",
+        },
+      },
+    };
+
+    this.averageBarChartThree = new Chart(ctx, {
+      type: "bar",
+      data: chartData,
+      options: chartOptions,
+    });
+  }
+
+  averageBarChartThreeData(responseData) {
+    let currentCallValue = responseData[0]["accumulated_calls"];
+    let currentCallAverage = responseData[0]["call_average"];
+
+    console.log(currentCallValue);
+    console.log(currentCallAverage);
+
+    // Remove Data
+    this.averageBarChartThree.data.datasets[0].data = [];
+    this.averageBarChartThree.data.datasets[1].data = [];
+    this.averageBarChartThree.data.datasets[0].backgroundColor = [];
+    this.averageBarChartThree.data.datasets[1].backgroundColor = [];
+    this.averageBarChartThree.update();
+    //
+
+    // Update Chart with new data
+    this.averageBarChartThree.data.datasets[0].data.push(currentCallValue);
+    this.averageBarChartThree.data.datasets[1].data.push(currentCallAverage);
+
+    this.averageBarChartThree.data.datasets[0].backgroundColor = "#32A9D9";
+    this.averageBarChartThree.data.datasets[1].backgroundColor =
+      "rgba(46, 78, 100, 1)";
+
+    this.averageBarChartThree.update();
+  }
+
+  // Units
+  createAverageBarChartFour() {
+    const ctx = document.querySelector("#average-chart-four").getContext("2d");
+
+    // Chart Data Display
+    let chartData = {
+      labels: ["Units"],
+      datasets: [
+        {
+          label: "Current Value",
+          backgroundColor: [],
+          data: [],
+        },
+        {
+          label: "Average",
+          backgroundColor: [],
+          data: [],
+        },
+      ],
+    };
+
+    // Chart Data Options
+    let chartOptions = {
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              fontColor: "white",
+            },
+            barPercentage: 0.7,
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: false,
+              labelString: "Categories",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              beginAtZero: true,
+              fontColor: "white",
+              max: 60,
+            },
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Units",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+      },
+      animation: false,
+      elements: {
+        line: {
+          tension: 0,
+        },
+      },
+      title: {
+        display: true,
+        text: "Units",
+        fontSize: 15,
+        fontColor: "white",
+      },
+      legend: {
+        labels: {
+          //   filter: function (item, chart) {
+          //     // Logic to remove a particular legend item goes here
+          //     return item.text == null || !item.text.includes("Current Value");
+          //   },
+          fontColor: "white",
+        },
+      },
+    };
+
+    this.averageBarChartFour = new Chart(ctx, {
+      type: "bar",
+      data: chartData,
+      options: chartOptions,
+    });
+  }
+
+  averageBarChartFourData(responseData) {
+    let currentUnitValue = responseData[0]["accumulated_units"];
+    let currentUnitAverage = responseData[0]["unit_average"];
+
+    // Remove Data
+    this.averageBarChartFour.data.datasets[0].data = [];
+    this.averageBarChartFour.data.datasets[1].data = [];
+    this.averageBarChartFour.data.datasets[0].backgroundColor = [];
+    this.averageBarChartFour.data.datasets[1].backgroundColor = [];
+    this.averageBarChartFour.update();
+    //
+
+    // Update Chart with new data
+    this.averageBarChartFour.data.datasets[0].data.push(currentUnitValue);
+    this.averageBarChartFour.data.datasets[1].data.push(currentUnitAverage);
+    this.averageBarChartFour.data.datasets[0].backgroundColor = "#32A9D9";
+    this.averageBarChartFour.data.datasets[1].backgroundColor =
+      "rgba(46, 78, 100, 1)";
+
+    this.averageBarChartFour.update();
+  }
+
+  // Level Zero
+  createAverageBarChartFive() {
+    const ctx = document.querySelector("#average-chart-five").getContext("2d");
+
+    // Chart Data Display
+    let chartData = {
+      labels: ["Level Zero"],
+      datasets: [
+        {
+          label: "Current Value",
+          backgroundColor: [],
+          data: [],
+        },
+        {
+          label: "Current Average",
+          backgroundColor: [],
+          data: [],
+        },
+      ],
+    };
+
+    // Chart Data Options
+    let chartOptions = {
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              fontColor: "white",
+            },
+            barPercentage: 0.7,
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: false,
+              labelString: "Categories",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            stacked: false,
+            ticks: {
+              fontSize: 12,
+              beginAtZero: true,
+              fontColor: "white",
+              max: 60,
+            },
+            gridLines: {
+              color: "rgba(255, 255, 255, 0.25)",
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Time(Minutes)",
+              fontColor: "white",
+              fontSize: 15,
+            },
+          },
+        ],
+      },
+      animation: false,
+      elements: {
+        line: {
+          tension: 0,
+        },
+      },
+      title: {
+        display: true,
+        text: "Level Zero",
+        fontSize: 15,
+        fontColor: "white",
+      },
+      legend: {
+        labels: {
+          //   filter: function (item, chart) {
+          //     // Logic to remove a particular legend item goes here
+          //     return item.text == null || !item.text.includes("Current Value");
+          //   },
+          fontColor: "white",
+        },
+      },
+    };
+
+    this.averageBarChartFive = new Chart(ctx, {
+      type: "bar",
+      data: chartData,
+      options: chartOptions,
+    });
+  }
+
+  averageBarChartFiveData(responseData) {
+    let currentLevelZeroValue = responseData[0]["accumulated_level_zero"];
+    let currentLevelZeroAverage = responseData[0]["level_zero_average"];
+
+    // Remove Data
+    this.averageBarChartFive.data.datasets[0].data = [];
+    this.averageBarChartFive.data.datasets[1].data = [];
+    this.averageBarChartFive.data.datasets[0].backgroundColor = [];
+    this.averageBarChartFive.data.datasets[1].backgroundColor = [];
+    this.averageBarChartFive.update();
+    //
+
+    // Update Chart with new data
+    this.averageBarChartFive.data.datasets[0].data.push(currentLevelZeroValue);
+    this.averageBarChartFive.data.datasets[1].data.push(currentLevelZeroAverage);
+    this.averageBarChartFive.data.datasets[0].backgroundColor = "#32A9D9";
+    this.averageBarChartFive.data.datasets[1].backgroundColor =
+      "rgba(46, 78, 100, 1)";
+
+    this.averageBarChartFive.update();
   }
 }
 
--- public/js/index.js
+++ public/js/index.js
@@ -7,52 +7,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
   master.fetchLiveWorkload();
 
   master.fetchSystem();
+
+  setInterval(() => {
+
+  }, 100)
 });
 
 class Master {
   constructor() {
     this.charts = charts;
-  }
-
-  // Fetches live workload data
-  fetchLiveWorkload() {
-    http
-      .get(`/live_workload`)
-      .then((data) => {
-        const { responseData, responseStatus } = data;
-
-        this.charts.liveWorkloadData(responseData);
-
-        const updatedAt = responseData[0].updated_at;
-
-        let fontColor;
-
-        fontColor = "yellow";
-
-        let updateAtElement = document.querySelector("#updated-at");
-
-        updateAtElement.style.color = fontColor;
-        updateAtElement.textContent = updatedAt;
-      })
-      .catch((err) => console.log(err));
-  }
-
-  // Fetches system averages data
-  fetchSystem() {
-    http
-      .get(`/system_report`)
-      .then((data) => {
-        const { responseData, responseStatus } = data;
-
-        this.charts.averageBarChartOneData(responseData);
-        this.charts.averageBarChartTwoData(responseData);
-      })
-      .catch((err) => console.log(err));
-  }
-}
-
-class unitWorkload {
-  constructor() {
     this.max_threshold = 1;
   }
 
@@ -82,7 +45,8 @@ class unitWorkload {
 
     let diff = (time_end - time_start) / 1000;
 
-    if (diff >= 120) {
+    // 5 minutes
+    if (diff >= 300) {
       bool = true;
     } else {
       bool = false;
@@ -91,7 +55,8 @@ class unitWorkload {
     return bool;
   }
 
-  fetchWorkload() {
+  // Fetches live workload data
+  fetchLiveWorkload() {
     http
       .get(`/live_workload`)
       .then((data) => {
@@ -99,23 +64,19 @@ class unitWorkload {
 
         const updatedAt = responseData[0].updated_at;
 
-        const bool = this.timeDifference(updatedAt);
+        let outdated = this.timeDifference(updatedAt);
 
         let fontColor;
 
-        if (bool) {
+        if (outdated) {
           fontColor = "red";
-          alert("SYSTEM IS DOWN");
+          alert("[OUTDATED DATA]");
         } else {
-          fontColor = "yellow";
-
+          this.charts.liveWorkloadData(responseData);
           this.systemCards(responseData);
-
           this.workloadTables(responseData);
-
-          this.workloadChart(responseData);
-
           this.crewList(responseData);
+          fontColor = "yellow";
         }
 
         let updateAtElement = document.querySelector("#updated-at");
@@ -126,14 +87,45 @@ class unitWorkload {
       .catch((err) => console.log(err));
   }
 
-  fetchReport() {
+  // Fetches system averages data
+  fetchSystem() {
     http
       .get(`/system_report`)
       .then((data) => {
         const { responseData, responseStatus } = data;
 
-        if (responseStatus === 200) {
-          this.systemReport(responseData);
+        if (responseData[0]["valid"]) {
+          let logOutput = "";
+
+          let systemLog = responseData[0]["systemLog"];
+
+          systemLog.forEach((log) => {
+            logOutput += `
+        <li>${log["log"]} [Driving: ${log["driving"]} - Posting: ${log["posting"]} - Level: ${log["level"]}]</li>
+      `;
+          });
+
+          if (logOutput === "") {
+            logOutput = "No Events Logged";
+          }
+
+          document.querySelector("#system-log").innerHTML = logOutput;
+
+          // Autoscroll to bottom of ul
+          const element = document.querySelector("#system-log");
+          element.scrollTop = element.scrollHeight - element.clientHeight;
+
+          this.charts.averageBarChartOneData(responseData);
+          this.charts.averageBarChartTwoData(responseData);
+          this.charts.averageBarChartThreeData(responseData);
+          this.charts.averageBarChartFourData(responseData);
+          this.charts.averageBarChartFiveData(responseData);
+
+          document.querySelector("#status").style.color = "yellow";
+          document.querySelector("#status").textContent = "Valid";
+        } else {
+          document.querySelector("#status").style.color = "red";
+          document.querySelector("#status").textContent = "Invalid";
         }
       })
       .catch((err) => console.log(err));
@@ -339,59 +331,6 @@ class unitWorkload {
     document.querySelector("#total-other").textContent = total_other;
   }
 
-  workloadChart(data) {
-    // Remove Data
-    this.barChart.data.labels = [];
-    this.barChart.data.datasets[0].data = [];
-    this.barChart.data.datasets[1].data = [];
-    this.barChart.data.datasets[2].data = [];
-    this.barChart.data.datasets[0].backgroundColor = [];
-    this.barChart.data.datasets[1].backgroundColor = [];
-    this.barChart.update();
-    //
-
-    // Sort data in descending order with threshold
-    data.sort((a, b) => {
-      return b.threshold - a.threshold;
-    });
-
-    data.forEach((row) => {
-      let unit = row.unit;
-      let current_threshold = row.threshold;
-      let current_workload = row.workload;
-
-      // Check if item already in chart data
-      if (
-        this.barChart.data.labels.includes(unit) !== true &&
-        unit !== undefined
-      ) {
-        if (current_workload >= 1) {
-          this.barChart.data.datasets[1].backgroundColor.push("red");
-          this.barChart.data.datasets[0].backgroundColor.push("#2E4E64");
-        } else if (current_workload > current_threshold) {
-          this.barChart.data.datasets[1].backgroundColor.push("yellow");
-          this.barChart.data.datasets[0].backgroundColor.push("#2E4E64");
-        } else {
-          this.barChart.data.datasets[1].backgroundColor.push("#32A9D9");
-          this.barChart.data.datasets[0].backgroundColor.push(
-            "rgba(46, 78, 100, 0.8)"
-          );
-        }
-
-        if (current_workload < 0) {
-          current_workload = 0;
-        }
-      }
-
-      // Update Chart with new data
-      this.barChart.data.labels.push(unit);
-      this.barChart.data.datasets[0].data.push(current_threshold);
-      this.barChart.data.datasets[1].data.push(current_workload);
-      this.barChart.data.datasets[2].data.push(1);
-      this.barChart.update();
-    });
-  }
-
   crewList(data) {
     let crews = "";
     let total = 0;
@@ -420,133 +359,6 @@ class unitWorkload {
     document.querySelector("#crew-list-tbody").innerHTML = crews;
     document.querySelector("#total-crews").textContent = total;
   }
-
-  timeConvert(num) {
-    let hours = num / 60;
-    let rhours = Math.floor(hours);
-    let minutes = (hours - rhours) * 60;
-    let rminutes = Math.round(minutes);
-    if (rminutes < 10) {
-      rminutes = `0${rminutes}`;
-    }
-    return rhours + ":" + rminutes;
-  }
-
-  systemReport(report) {
-    if (report[0]["valid"] === true) {
-      document.querySelector("#top-system-report").style.display = "grid";
-
-      document.querySelector("#status").style.color = "yellow";
-      document.querySelector("#status").textContent = "Valid";
-
-      let logOutput = "";
-
-      let systemLog = report[0]["systemLog"];
-
-      systemLog.forEach((log) => {
-        logOutput += `
-        <li>${log["log"]} [Driving: ${log["driving"]} - Posting: ${log["posting"]} - Level: ${log["level"]}]</li>
-      `;
-      });
-
-      if (logOutput === "") {
-        logOutput = "No Events Logged";
-      }
-
-      document.querySelector("#system-log").innerHTML = logOutput;
-
-      // Autoscroll to bottom of ul
-      const element = document.querySelector("#system-log");
-      element.scrollTop = element.scrollHeight - element.clientHeight;
-
-      let accumulated_calls = report[0]["accumulated_calls"];
-      let accumulated_post_time = report[0]["accumulated_post_time"];
-      let accumulated_units = report[0]["accumulated_units"];
-      let accumulated_drive_time = report[0]["accumulated_drive_time"];
-      let accumulated_on_call_time = report[0]["accumulated_on_call_time"];
-      let accumulated_late_calls = report[0]["accumulated_late_calls"];
-      let accumulated_past_eos = report[0]["accumulated_past_eos"];
-      let accumulated_level_zero = report[0]["accumulated_level_zero"];
-
-      let call_status = report[0]["call_status"];
-      let on_call_status = report[0]["on_call_status"];
-      let post_time_status = report[0]["post_time_status"];
-      let drive_time_status = report[0]["drive_time_status"];
-      let unit_status = report[0]["unit_status"];
-      let past_eos_status = report[0]["past_eos_status"];
-      let late_call_status = report[0]["late_call_status"];
-      let level_zero_status = report[0]["level_zero_status"];
-
-      let call_average = report[0]["call_average"];
-      let on_call_average = report[0]["on_call_average"];
-      let post_time_average = report[0]["post_time_average"];
-      let drive_time_average = report[0]["drive_time_average"];
-      let unit_average = report[0]["unit_average"];
-      let past_eos_average = report[0]["past_eos_average"];
-      let late_call_average = report[0]["late_call_average"];
-      let level_zero_average = report[0]["level_zero_average"];
-
-      document.querySelector("#total-calls").textContent =
-        accumulated_calls +
-        " - " +
-        call_status +
-        " (Average: " +
-        call_average +
-        ")";
-      document.querySelector("#total-post-time").textContent =
-        this.timeConvert(accumulated_post_time) +
-        " - " +
-        post_time_status +
-        " (Average: " +
-        this.timeConvert(post_time_average) +
-        ")";
-      document.querySelector("#total-units").textContent =
-        accumulated_units +
-        " - " +
-        unit_status +
-        " (Average: " +
-        unit_average +
-        ")";
-      document.querySelector("#total-drive-time").textContent =
-        this.timeConvert(accumulated_drive_time) +
-        " - " +
-        drive_time_status +
-        " (Average: " +
-        this.timeConvert(drive_time_average) +
-        ")";
-      document.querySelector("#total-on-call-time").textContent =
-        this.timeConvert(accumulated_on_call_time) +
-        " - " +
-        on_call_status +
-        " (Average: " +
-        this.timeConvert(on_call_average) +
-        ")";
-      document.querySelector("#total-late-calls").textContent =
-        accumulated_late_calls +
-        " - " +
-        late_call_status +
-        " (Average: " +
-        late_call_average +
-        ")";
-      document.querySelector("#total-past-eos").textContent =
-        accumulated_past_eos +
-        " - " +
-        past_eos_status +
-        " (Average: " +
-        past_eos_average +
-        ")";
-      document.querySelector("#total-time-level-zero").textContent =
-        this.timeConvert(accumulated_level_zero) +
-        " - " +
-        level_zero_status +
-        " (Average: " +
-        this.timeConvert(level_zero_average) +
-        ")";
-    } else {
-      document.querySelector("#top-system-report").style.display = "none";
-
-      document.querySelector("#status").style.color = "red";
-      document.querySelector("#status").textContent = "Invalid";
-    }
-  }
 }
+
+
